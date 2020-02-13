fc502c53de218b802b7085b158cc7a2bc6e5cfa4
Lucas Caparelli <lucas.caparelli112@gmail.com>
2020-02-13 10:54:52 -0300

    Implement divide and conquer approach.
    Implement DnC approach to improve time complexity -- O(log n).
    
    Signed-off-by: Lucas Caparelli <lucas.caparelli112@gmail.com>

--- class_1/peak_finder/peakfinder_v1.py
+++ class_1/peak_finder/peakfinder_v1.py
@@ -17,7 +17,8 @@ With this constraint and definition of peak there is always at least one peak.
 """
 
 
-def peak_finder(array):
+# O(n)
+def peak_finder_straightforward(array):
     if len(array) == 1:
         return 0
 
@@ -32,24 +33,57 @@ def peak_finder(array):
             return i
 
 
+# O(log_2 n) == O(log n)
+def peak_finder_divide_n_conquer(idx, array):
+    if is_valid_idx(idx - 1, array) and array[idx - 1] > array[idx]:
+        return peak_finder_divide_n_conquer(int((idx - 1) / 2), array)
+    elif is_valid_idx(idx + 1, array) and array[idx + 1] > array[idx]:
+        return peak_finder_divide_n_conquer(int((idx + 1 + len(array)) / 2), array)
+    else:
+        return idx
+
+
 def is_valid_idx(idx, array):
     return True if 0 <= idx < len(array) else False
 
 
 if __name__ == '__main__':
-    # expected 0
-    print(peak_finder([1]))
-    # expected 1
-    print(peak_finder([1, 2]))
-    # expected 0
-    print(peak_finder([1, 0]))
-    # expected 3
-    print(peak_finder([0, 1, 2, 3, 3]))
-    # expected 3
-    print(peak_finder([0, 1, 2, 3, 2]))
-    # expected 1
-    print(peak_finder([0, 1, 1]))
-    # expected 0
-    print(peak_finder([0, 0]))
-    # expected 2
-    print(peak_finder([0, 1, 2, 1, 1, 0, -1, -2]))
+    print("\nExpected: " + str(0))
+    array = [1]
+    print("Straightforward: " + str(peak_finder_straightforward(array)))
+    print("Divide and conquer: " + str(peak_finder_divide_n_conquer(int(len(array) / 2), array)))
+
+    print("\nExpected: " + str(1))
+    array = [1, 2]
+    print("Straightforward: " + str(peak_finder_straightforward(array)))
+    print("Divide and conquer: " + str(peak_finder_divide_n_conquer(int(len(array) / 2), array)))
+
+    print("\nExpected: " + str(0))
+    array = [1, 0]
+    print("Straightforward: " + str(peak_finder_straightforward(array)))
+    print("Divide and conquer: " + str(peak_finder_divide_n_conquer(int(len(array) / 2), array)))
+
+    print("\nExpected: " + str(3) + " or " + str(4))
+    array = [0, 1, 2, 3, 3]
+    print("Straightforward: " + str(peak_finder_straightforward(array)))
+    print("Divide and conquer: " + str(peak_finder_divide_n_conquer(int(len(array) / 2), array)))
+
+    print("\nExpected: " + str(3))
+    array = [0, 1, 2, 3, 2]
+    print("Straightforward: " + str(peak_finder_straightforward(array)))
+    print("Divide and conquer: " + str(peak_finder_divide_n_conquer(int(len(array) / 2), array)))
+
+    print("\nExpected: " + str(1) + " or " + str(2))
+    array = [0, 1, 1]
+    print("Straightforward: " + str(peak_finder_straightforward(array)))
+    print("Divide and conquer: " + str(peak_finder_divide_n_conquer(int(len(array) / 2), array)))
+
+    print("\nExpected: " + str(0) + " or " + str(1))
+    array = [0, 0]
+    print("Straightforward: " + str(peak_finder_straightforward(array)))
+    print("Divide and conquer: " + str(peak_finder_divide_n_conquer(int(len(array) / 2), array)))
+
+    print("\nExpected: " + str(2) + " or " + str(4))
+    array = [0, 1, 2, 1, 1, 0, -1, -2]
+    print("Straightforward: " + str(peak_finder_straightforward(array)))
+    print("Divide and conquer: " + str(peak_finder_divide_n_conquer(int(len(array) / 2), array)))
